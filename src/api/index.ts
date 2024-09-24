f8cf96ce91dd278c0b0cce6455bb6206e395235a
ID <54764772+LL1yt@users.noreply.github.com>
2024-09-24 22:01:40 +0300

    delete unused api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,7 @@
 import { Anthropic } from "@anthropic-ai/sdk"
 import { ApiConfiguration, ModelInfo } from "../shared/api"
 import { AnthropicHandler } from "./anthropic"
-import { AwsBedrockHandler } from "./bedrock"
-import { OpenRouterHandler } from "./openrouter"
-import { VertexHandler } from "./vertex"
 import { OpenAiHandler } from "./openai"
-import { OllamaHandler } from "./ollama"
-import { GeminiHandler } from "./gemini"
-import { OpenAiNativeHandler } from "./openai-native"
 
 export interface ApiHandlerMessageResponse {
 	message: Anthropic.Messages.Message
@@ -29,20 +23,9 @@ export function buildApiHandler(configuration: ApiConfiguration): ApiHandler {
 	switch (apiProvider) {
 		case "anthropic":
 			return new AnthropicHandler(options)
-		case "openrouter":
-			return new OpenRouterHandler(options)
-		case "bedrock":
-			return new AwsBedrockHandler(options)
-		case "vertex":
-			return new VertexHandler(options)
 		case "openai":
 			return new OpenAiHandler(options)
 		case "ollama":
-			return new OllamaHandler(options)
-		case "gemini":
-			return new GeminiHandler(options)
-		case "openai-native":
-			return new OpenAiNativeHandler(options)
 		default:
 			return new AnthropicHandler(options)
 	}
